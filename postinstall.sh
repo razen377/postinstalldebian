b1f42340ebf54e4a8d2b7e7f34ca691aeebfac95
razen377 <deplaixguillaume@gmail.com>
2025-12-10 17:09:33 +0100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-# Script ultra-simple – 100% automatique – marche partout
-
-echo "Mise à jour et installation en cours... (Cela peut prendre quelques minutes)"
-
-# On ouvre un bloc avec { } et on redirige tout ce qui est dedans vers /dev/null
-{
-    # Évite les questions de configuration interactives pendant l'install
-    export DEBIAN_FRONTEND=noninteractive
 
     apt update && apt upgrade -y
     apt install -y ssh zip unzip nmap locate ncdu curl git screen dnsutils net-tools sudo lynx winbind samba
@@ -14,13 +6,10 @@ echo "Mise à jour et installation en cours... (Cela peut prendre quelques minut
     # Indexation pour locate
     updatedb
 
-    # Ajout automatique de "wins" dans nsswitch.conf (si pas déjà là)
+    # Ajout automatique de "wins" 
     grep -q "wins" /etc/nsswitch.conf || sed -i '/^hosts:/ s/$/ wins/' /etc/nsswitch.conf
 
-    # Activer le beau prompt couleur pour root
     sed -i '9,13s/^#//' /root/.bashrc
-
-} > /dev/null 2>&1
 
 # Les commandes ci-dessous s'afficheront car elles sont hors du bloc silencieux
 echo ""
