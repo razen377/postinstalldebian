94ee0f1008a2380fcc1ca78a1db051b9f1d14543
razen377 <deplaixguillaume@gmail.com>
2025-12-10 17:24:54 +0100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
-
+    # Installation
     apt update && apt upgrade -y
     apt install -y ssh zip unzip nmap locate ncdu curl git screen dnsutils net-tools sudo lynx winbind samba
 
-    # Indexation pour locate
-    updatedb
+    # 1. Ajout des alias de coloration syntaxique à /root/.bashrc
+    echo "export LS_OPTIONS='--color=auto'" >> /root/.bashrc
+    echo "eval \"\$(dircolors)\"" >> /root/.bashrc
+    echo "alias ls='ls \$LS_OPTIONS'" >> /root/.bashrc
+    echo "alias ll='ls \$LS_OPTIONS -l'" >> /root/.bashrc
 
-    # Ajout automatique de "wins" 
-    grep -q "wins" /etc/nsswitch.conf || sed -i '/^hosts:/ s/$/ wins/' /etc/nsswitch.conf
+    # --- Section de configuration de nsswitch.conf ---
+    # 2. Suppression du fichier existant
+    rm -f /etc/nsswitch.conf
 
-    sed -i '9,13s/^#//' /root/.bashrc
+    # 3. Recréation du fichier avec le contenu spécifié
+    cat /etc/nsswitch.conf
+
+passwd:          files systemd winbind
+group:           files systemd winbind
+shadow:          files systemd
+gshadow:         files systemd
+
+hosts:           files dns wins
+networks:        files
+
+protocols:       db files
+services:        db files
+ethers:          db files
+rpc:             db files
+
+netgroup:        nis
+
+    # --------------------------------------------------
 
 # Les commandes ci-dessous s'afficheront car elles sont hors du bloc silencieux
 echo ""
